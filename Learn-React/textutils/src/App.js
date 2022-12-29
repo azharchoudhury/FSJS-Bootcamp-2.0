c2a005890266149975c7755d21084dbb770e1452
azharchoudhury <azharmehmud11@gmail.com>
2022-12-30 01:02:14 +0530

    React basics practice codes

--- Learn-React/textutils/src/App.js
+++ Learn-React/textutils/src/App.js
@@ -2,28 +2,58 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // const  name = "Azhar";
+  // const friend = <h3>Abhirup</h3>
+  // const age = <h3>25</h3>;
+  // const email = <h3>azhar@webdev.com</h3>;
+  // const user = (
+    // <div>
+      {/* {name}
+      {age}
+      {email} */}
+    // </div>
+  // )
+
   return (
-    <>
-    <nav>
-      <li>Home</li>
-      <li>Services</li>
-      <li>About</li>
-      <li>Contact Us</li>
-    </nav>
+   <div className="App">
 
-    <br />
+    {/* <User/>
+    <User/>
+    <User/> */}
 
-    <div className="container">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat illum quae est neque hic tempore eum velit quia minus. Quo vitae labore alias quod est at officia ad aliquid deleniti!
-    </div>
+    {/* <User name="Azhar" age={25} email="azhar@life.com" />
+    <User name="Abhirup" age={25} email="abhirup@life.com" />
+    <User name="Bhargav" age={25} email="bhargav@life.com" /> */}
 
-    <br />
+    <Job salary = {1400000} position = "Senior SDE" company="Amazon"/>
+    <Job salary = {2400000} position = "Project Manager" company="Google"/>
+    <Job salary = {1200000} position = "Junior SDE" company="Razorpay"/>
 
-    <div style={{fontWeight: "bold"}}>
-      Hello Azhar
-    </div>
-    </>
+
+   </div>
   );
+}
+
+const Job = (props) =>{
+  return(
+    <div>
+      <h1>{props.salary}</h1>
+      <h2>{props.position}</h2>
+      <h3>{props.company}</h3>
+    </div>
+  )
+}
+
+
+
+const User = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.age}</h2>
+      <h3>{props.email}</h3>
+    </div>
+  )
 }
 
 export default App;
