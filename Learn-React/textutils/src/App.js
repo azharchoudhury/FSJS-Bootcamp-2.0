40fd8221cf8f3fede2b47499f94b1d413293691b
azharchoudhury <azharmehmud11@gmail.com>
2022-12-29 12:13:16 +0530

    JS refresher file added

---
+++ Learn-React/textutils/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <nav>
+      <li>Home</li>
+      <li>Services</li>
+      <li>About</li>
+      <li>Contact Us</li>
+    </nav>
+
+    <br />
+
+    <div className="container">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat illum quae est neque hic tempore eum velit quia minus. Quo vitae labore alias quod est at officia ad aliquid deleniti!
+    </div>
+
+    <br />
+
+    <div style={{fontWeight: "bold"}}>
+      Hello Azhar
+    </div>
+    </>
+  );
+}
+
+export default App;
