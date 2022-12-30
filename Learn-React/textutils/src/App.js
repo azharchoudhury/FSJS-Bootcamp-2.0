316705a684e0b8f2338420b4c70b8eba20747e53
azharchoudhury <azharmehmud11@gmail.com>
2022-12-30 09:44:19 +0530

    new codes added

--- Learn-React/textutils/src/App.js
+++ Learn-React/textutils/src/App.js
@@ -16,7 +16,11 @@ function App() {
 
 
   const age = 22;
-  const isGreen = false;
+  const isGreen = true;
+  const userName = "David";
+  const names = ["Pedro", "Jake", "Jessica", "Mike", "Dustin"];
+
+
 
   // if(age >= 18){
   //   return <h1>Over Age</h1>
@@ -33,6 +37,7 @@ function App() {
     <User/>
     <User/> */}
 
+    {/* Proops */}
     {/* <User name="Azhar" age={25} email="azhar@life.com" />
     <User name="Abhirup" age={25} email="abhirup@life.com" />
     <User name="Bhargav" age={25} email="bhargav@life.com" /> */}
@@ -42,13 +47,23 @@ function App() {
     <Job salary = {2400000} position = "Project Manager" company="Google"/>
     <Job salary = {1200000} position = "Junior SDE" company="Razorpay"/> */}
 
-    {age >= 18 ? <h1>Over Age</h1> : <h2>Under Age</h2>} 
+
     {/* Ternary operator */}
+    {age >= 18 ? <h1>Over Age</h1> : <h2>Under Age</h2>} 
     <h1 style={{color: isGreen ? "green" : "red"}}>This has color.</h1>
+
+    {isGreen && <button>Login</button>}
+
 
     <br/>
     <br/>
     <br/>
+
+    {/* Map */}
+
+    {names.map((name, index) => {
+       return <h4 key={index}>{name}</h4>
+    })}
 
     
 
@@ -56,26 +71,27 @@ function App() {
   );
 }
 
-const Job = (props) =>{
-  return(
-    <div>
-      <h1>{props.salary}</h1>
-      <h2>{props.position}</h2>
-      <h3>{props.company}</h3>
-    </div>
-  )
-}
+// Props
+// const Job = (props) =>{
+//   return(
+//     <div>
+//       <h1>{props.salary}</h1>
+//       <h2>{props.position}</h2>
+//       <h3>{props.company}</h3>
+//     </div>
+//   )
+// }
 
 
 
-const User = (props) => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.age}</h2>
-      <h3>{props.email}</h3>
-    </div>
-  )
-}
+// const User = (props) => {
+//   return (
+//     <div>
+//       <h1>{props.name}</h1>
+//       <h2>{props.age}</h2>
+//       <h3>{props.email}</h3>
+//     </div>
+//   )
+// }
 
 export default App;
